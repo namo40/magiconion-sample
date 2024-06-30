af9de975f3f02912cc83ceb823f96a4efd3a817f
namo4 <namo40@naver.com>
2024-06-30 18:06:58 +0900

    Cli 프로젝트 추가

---
+++ MagicOnionSample.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleAppFramework;
+
+var app = ConsoleApp.Create();
+
+var isRunning = true;
+
+app.Add("exit", () => isRunning = false);
+
+while (isRunning)
+{
+    Console.Write("MagicOnionSample.Cli> ");
+    var readLine = Console.ReadLine();
+    if (string.IsNullOrEmpty(readLine))
+        continue;
+
+    await app.RunAsync(readLine.Split(" "));
+}
