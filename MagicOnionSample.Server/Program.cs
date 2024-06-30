ebc9d1c060fe78c6199a98806707cfd4da419e66
namo4 <namo40@naver.com>
2024-06-30 17:44:06 +0900

    MagicOnion 서비스 등록

--- MagicOnionSample.Server/Program.cs
+++ MagicOnionSample.Server/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
+builder.WebHost.ConfigureKestrel(options =>
+    options.ConfigureEndpointDefaults(endpointOptions => endpointOptions.Protocols = HttpProtocols.Http2));
+
+// Grpc Service 등록
+builder.Services.AddGrpc();
+// MagicOnion Service 등록
+builder.Services.AddMagicOnion();
+
 var app = builder.Build();
+
+// MagicOnion용 Grpc Service 맵핑
+app.MapMagicOnionService();
 
 app.MapGet("/", () => "Hello World!");
 
